0b262717cc60045ac9d781638e688471b39fab84
Pavel Fomchenkov <hello@pavel.codes>
2022-06-09 14:40:34 +0200

    Get rid of helper functions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
-import NextAuth, { Profile, TokenSet } from "next-auth"
+import NextAuth from "next-auth"
 import {
   TokenEndpointHandler,
   UserinfoEndpointHandler,
 } from "next-auth/providers"
+import { AzureDevOpsProfile } from "../../providers/azure-devops"
 
 const clientId = process.env.AZURE_DEVOPS_APP_ID
 const clientSecret = process.env.AZURE_DEVOPS_CLIENT_SECRET
@@ -24,13 +25,36 @@ export default NextAuth({
       token: {
         url: "https://app.vssps.visualstudio.com/oauth2/token",
         async request(context) {
-          return { tokens: await makeTokenRequest(context) }
+          const tokenEndpoint = context.provider.token as TokenEndpointHandler
+          const response = await fetch(tokenEndpoint.url as string, {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            method: "POST",
+            body: new URLSearchParams({
+              client_assertion_type:
+                "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+              client_assertion: context.provider.clientSecret as string,
+              grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+              assertion: context.params.code as string,
+              redirect_uri: context.provider.callbackUrl,
+            }),
+          })
+          return { tokens: await response.json() }
         },
       },
       userinfo: {
         url: "https://app.vssps.visualstudio.com/_apis/profile/profiles/me?details=true&coreAttributes=Avatar&api-version=6.0",
         async request(context) {
-          return await makeUserInfoRequest(context)
+          const accessToken = context.tokens.access_token as string
+          const userInfoEndpoint = context.provider
+            .userinfo as UserinfoEndpointHandler
+          const response = await fetch(userInfoEndpoint.url as string, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+          return response.json()
         },
       },
       async profile(profile: AzureDevOpsProfile) {
@@ -46,78 +70,3 @@ export default NextAuth({
     },
   ],
 })
-
-// USER INFO
-
-interface AzureDevOpsProfile extends Profile {
-  id: string
-  displayName: string
-  emailAddress: string
-  coreAttributes: {
-    Avatar: {
-      value: {
-        value: string
-      }
-    }
-  }
-}
-
-async function makeUserInfoRequest(
-  context: UserInfoRequestContext
-): Promise<AzureDevOpsProfile> {
-  const accessToken = context.tokens.access_token as string
-  const userInfoEndpoint = context.provider.userinfo as UserinfoEndpointHandler
-  const response = await fetch(userInfoEndpoint.url as string, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-  return response.json()
-}
-
-interface UserInfoRequestContext {
-  tokens: TokenSet
-  provider: {
-    userinfo?: string | UserinfoEndpointHandler
-  }
-}
-
-// TOKEN
-
-async function makeTokenRequest(
-  context: TokenRequestContext
-): Promise<TokenSet> {
-  const tokenEndpoint = context.provider.token as TokenEndpointHandler
-  const response = await fetch(tokenEndpoint.url as string, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    method: "POST",
-    body: createTokenRequestBody(context),
-  })
-  const tokenResponse = await response.json()
-  return tokenResponse as TokenSet
-}
-
-interface TokenRequestContext {
-  params: { code?: string; state?: string }
-  provider: {
-    token?: string | TokenEndpointHandler
-    clientSecret?: string
-    callbackUrl?: string
-  }
-}
-
-function createTokenRequestBody(context: TokenRequestContext) {
-  const clientSecret = context.provider.clientSecret as string
-  const callbackUrl = context.provider.callbackUrl as string
-  const code = context.params.code as string
-  return new URLSearchParams({
-    client_assertion_type:
-      "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-    client_assertion: clientSecret,
-    grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-    assertion: code,
-    redirect_uri: callbackUrl,
-  })
-}
---
+++ pages/providers/azure-devops.ts
@@ -0,0 +1,69 @@
+import { OAuthConfig, OAuthUserConfig } from "next-auth/providers"
+
+export interface AzureDevOpsProfile extends Record<string, unknown> {
+  id: string
+  displayName: string
+  emailAddress: string
+  coreAttributes: {
+    Avatar: {
+      value: {
+        value: string
+      }
+    }
+  }
+}
+
+export default function AzureDevOps<P extends AzureDevOpsProfile>(
+  options: OAuthUserConfig<P> & {
+    /**
+     * https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples
+     * @default 48
+     */
+    profilePhotoSize?: 48 | 64 | 96 | 120 | 240 | 360 | 432 | 504 | 648
+  }
+): OAuthConfig<P> {
+  const profilePhotoSize = options.profilePhotoSize ?? 48
+
+  return {
+    id: "azure-ad",
+    name: "Azure Active Directory",
+    type: "oauth",
+    wellKnown: `https://login.microsoftonline.com/${tenant}/v2.0/.well-known/openid-configuration`,
+    authorization: {
+      params: {
+        scope: "openid profile email",
+      },
+    },
+
+    async profile(profile, tokens) {
+      // https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples
+      const profilePicture = await fetch(
+        `https://graph.microsoft.com/v1.0/me/photos/${profilePhotoSize}x${profilePhotoSize}/$value`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokens.access_token}`,
+          },
+        }
+      )
+
+      // Confirm that profile photo was returned
+      if (profilePicture.ok) {
+        const pictureBuffer = await profilePicture.arrayBuffer()
+        const pictureBase64 = Buffer.from(pictureBuffer).toString("base64")
+        return {
+          id: profile.id,
+          name: profile.displayName,
+          email: profile.emailAddress,
+          image: `data:image/jpeg;base64, ${pictureBase64}`,
+        }
+      } else {
+        return {
+          id: profile.id,
+          name: profile.displayName,
+          email: profile.emailAddress,
+        }
+      }
+    },
+    options,
+  }
+}
